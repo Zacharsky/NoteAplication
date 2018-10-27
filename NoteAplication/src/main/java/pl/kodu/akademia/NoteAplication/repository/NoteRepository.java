5033cfa581d4f19f7331889d0cf9e4c7b578a34e
Zacharsky <mateusz.zacharski1@gmail.com>
2018-10-27 12:02:24 +0200

    Commit3

---
+++ NoteAplication/src/main/java/pl/kodu/akademia/NoteAplication/repository/NoteRepository.java
@@ -0,0 +1,10 @@
+package pl.kodu.akademia.NoteAplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.kodu.akademia.NoteAplication.model.Note;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    
+}
