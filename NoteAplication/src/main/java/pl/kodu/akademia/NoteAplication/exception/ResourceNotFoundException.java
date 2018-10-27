dcda1eb3961a89312a5bbad718389839cd8a3996
Zacharsky <mateusz.zacharski1@gmail.com>
2018-10-27 12:12:07 +0200

    Commit 4

--- NoteAplication/src/main/java/pl/kodu/akademia/NoteAplication/exception/ResourceNotFoundException.java
+++ NoteAplication/src/main/java/pl/kodu/akademia/NoteAplication/exception/ResourceNotFoundException.java
@@ -4,5 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+
+    private String resurceName;
+    private String fileName;
+    private Object fileValue;
+
+    private ResourceNotFoundException(String resurceName, String fileName, Object fileValue){
+
+        super(String.format("%s not fund with %s : '%s'", resurceName, fileName, fileValue));
+        this.resurceName = resurceName;
+        this.fileName = fileName;
+        this.fileValue = fileValue;
+
+    }
 }
