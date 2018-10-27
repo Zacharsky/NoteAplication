1e353a4d9ddf442014e28e3242835784510bf896
Zacharsky <mateusz.zacharski1@gmail.com>
2018-10-27 10:28:49 +0200

    Commit1

---
+++ NoteAplication/src/main/java/pl/kodu/akademia/NoteAplication/NoteAplicationApplication.java
@@ -0,0 +1,12 @@
+package pl.kodu.akademia.NoteAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteAplicationApplication.class, args);
+	}
+}
---
+++ NoteAplication/src/test/java/pl/kodu/akademia/NoteAplication/NoteAplicationApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.kodu.akademia.NoteAplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoteAplicationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
